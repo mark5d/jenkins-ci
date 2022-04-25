90e7f7008720bc9559df2c420656890a69da82c9
Administrator <admin@example.com>
2022-04-25 17:00:02 +0800

    ok

---
+++ src/org/devops/ci-config.groovy
@@ -0,0 +1,14 @@
+package org.devops 
+
+config {
+    imageRepository = "11111"
+
+    giblabHost = ""
+
+    // buildingContainer {
+    //     jdk8-maven3u5u0 = "111111111111"
+    // }
+    
+}
+
+
