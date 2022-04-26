c3f2cb9dffb46abf49c76b9eb4322acb0d69e4bb
Administrator <admin@example.com>
2022-04-26 11:43:53 +0800

    ok

--- src/org/devops/ci_config.groovy
+++ src/org/devops/ci_config.groovy
@@ -16,9 +16,9 @@ package org.devops
 class ci_config {
     def imageRepository = "https://harbor.templete.com/"
 
-    def gitlabApiHost = "http://gitlab-headless.defult/api/v4"
+    def gitlabApiHost = "http://gitlab-headless.default/api/v4"
     def gitlabIngressHost = "gitlab.example.com"
-    def gitlabHeadlessHost = "gitlab-headless.defult"
+    def gitlabHeadlessHost = "gitlab-headless.default"
     def gitlabAccountCredentialsId = "root"
 
     def pipelineWith =[ "main" , "qa" , "dev" ]
